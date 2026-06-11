7e527f9147d85b77c83938fff5fd08a2ba476d3e
bouraissiayoub <bouraissiayoub1@gmail.com>
2026-06-11 16:40:16 +0200

    admin access fixed

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
+use Symfony\Component\HttpFoundation\Request;
 #[Route('/admin')]
 class AdminController extends AbstractController
 {
